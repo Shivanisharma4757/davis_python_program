09d4a8399111d72bbae9df5444a662ca5260df92
Shivanisharma4757 <shivani.sharma.mca.2025@miet.ac.in>
2026-02-21 19:17:18 +0530

    Add banking transaction verification logic

---
+++ selaction_statement_lab1/banking.py
@@ -0,0 +1,10 @@
+# Input
+amount = float(input("Enter transaction amount: "))
+account_age = int(input("Enter account age in months: "))
+international = input("Is the transaction international? (yes/no): ").lower()
+
+# Condition check
+if amount > 200000 and account_age < 6 and international == "yes":
+    print("Transaction Flagged for Manual Verification.")
+else:
+    print("Transaction is Normal.")
