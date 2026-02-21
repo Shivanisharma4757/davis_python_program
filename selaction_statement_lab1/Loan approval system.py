70846d04eb59a12ef04fbd71dceba09053ff99ff
Shivanisharma4757 <shivani.sharma.mca.2025@miet.ac.in>
2026-02-21 19:23:56 +0530

    Add Loan Approval System Program
    Implement loan approval logic based on credit score, income, and existing loans.

---
+++ selaction_statement_lab1/Loan approval system.py
@@ -0,0 +1,23 @@
+# Loan Approval System Program
+
+# Taking input
+credit_score = int(input("Enter credit score: "))
+monthly_income = float(input("Enter monthly income: "))
+existing_loans = float(input("Enter total existing loan amount: "))
+
+# Checking credit score first
+
+if credit_score < 600:
+    print("Loan Rejected: Low Credit Score.")
+
+elif credit_score >= 750:
+    print("Loan Approved.")
+
+else:
+    # For credit score between 600 and 749
+    # Additional income and loan check required
+    
+    if monthly_income < 30000 and existing_loans > 500000:
+        print("Loan Rejected: Low Income and High Existing Loans.")
+    else:
+        print("Loan Approved after Income Verification.")
