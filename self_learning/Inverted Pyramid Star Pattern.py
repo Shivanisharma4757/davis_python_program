bede25f68a622c9020d1d07ee70513dd1c3937bd
Shivanisharma4757 <shivani.sharma.mca.2025@miet.ac.in>
2026-02-22 19:17:53 +0530

    Create Inverted Pyramid Star Pattern.py

---
+++ self_learning/Inverted Pyramid Star Pattern.py
@@ -0,0 +1,15 @@
+# Number of rows
+n = 5
+
+for i in range(n, 0, -1):
+    # Print leading spaces
+    print(' ' * (n - i), end='')
+    # Print stars
+    print('*' * (2 * i - 1))
+
+output
+*********
+ *******
+  *****
+   ***
+    *
