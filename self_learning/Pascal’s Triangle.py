868bf49fc7480cbe0a35d5fcfebc73043b635416
Shivanisharma4757 <shivani.sharma.mca.2025@miet.ac.in>
2026-02-22 19:37:23 +0530

    Add Pascal's Triangle implementation
    Implement Pascal's Triangle generation with specified number of rows.

---
+++ self_learning/Pascal’s Triangle.py
@@ -0,0 +1,20 @@
+# Number of rows
+n = 5
+
+for i in range(n):
+    # Print leading spaces
+    print(' ' * (n - i - 1), end='')
+    
+    # Calculate and print values in the row
+    val = 1
+    for j in range(i + 1):
+        print(val, end=' ')
+        val = val * (i - j) // (j + 1)  # Compute next value
+    print()  # Move to next line
+
+output
+    1
+   1 1
+  1 2 1
+ 1 3 3 1
+1 4 6 4 1
