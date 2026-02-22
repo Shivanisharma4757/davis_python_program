7956ff63da8a2910656ad3459abfd67e450724d9
Shivanisharma4757 <shivani.sharma.mca.2025@miet.ac.in>
2026-02-22 19:20:16 +0530

    Create Diamond Star Pattern.py

---
+++ self_learning/Diamond Star Pattern.py
@@ -0,0 +1,21 @@
+# Number of rows for the top half
+n = 5
+
+# Top pyramid
+for i in range(1, n + 1):
+    print(' ' * (n - i) + '*' * (2 * i - 1))
+
+# Bottom inverted pyramid
+for i in range(n - 1, 0, -1):
+    print(' ' * (n - i) + '*' * (2 * i - 1))
+
+output
+    *
+   ***
+  *****
+ *******
+*********
+ *******
+  *****
+   ***
+    *
