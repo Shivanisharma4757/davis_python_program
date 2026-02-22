761ad0c372a3cfb8ac4d9faf6685884915afadf0
Shivanisharma4757 <shivani.sharma.mca.2025@miet.ac.in>
2026-02-22 18:35:44 +0530

    Create reverse of anumber.py

---
+++ iteratin_statement/reverse of anumber.py
@@ -0,0 +1,19 @@
+# Program to find the reverse of a number using a while loop
+
+# Take input from the user
+num = int(input("Enter a number: "))
+
+# Store original number (optional, for display purposes)
+original = num
+
+# Initialize variable to store reversed number
+reverse = 0
+
+# Loop until the number becomes 0
+while num > 0:
+    digit = num % 10          # Get the last digit
+    reverse = reverse * 10 + digit  # Append digit to reverse
+    num = num // 10           # Remove the last digit
+
+# Display the reversed number
+print("Reverse of", original, "is", reverse)
