bcd82b20ae0da45f0c4c0f30ad63ea035fceabff
Shivanisharma4757 <shivani.sharma.mca.2025@miet.ac.in>
2026-02-23 20:37:55 +0530

    Update multiplication table.py

--- iteratin_statement/multiplication table.py
+++ iteratin_statement/multiplication table.py
@@ -12,3 +12,17 @@ print(f"Multiplication table of {num} up to {limit}:")
 for i in range(1, limit + 1):
     result = num * i
     print(f"{num} x {i} = {result}")
+#output
+#Enter a number: 5
+#Enter the range of the table: 10
+#Multiplication table of 5 up to 10:
+#5 x 1 = 5
+#5 x 2 = 10
+#5 x 3 = 15
+#5 x 4 = 20
+#5 x 5 = 25
+#5 x 6 = 30
+#5 x 7 = 35
+#5 x 8 = 40
+#5 x 9 = 45
+#5 x 10 = 50
