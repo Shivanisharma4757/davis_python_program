09de8c0ad5b47b835725cde384bebaab2d79ca01
Shivanisharma4757 <shivani.sharma.mca.2025@miet.ac.in>
2026-02-23 20:00:08 +0530

    Create Add _and _update _dictionary _items.py

---
+++ 23_feb_self_learning_lab_work/Add _and _update _dictionary _items.py
@@ -0,0 +1,17 @@
+# Create dictionary
+car = {
+    "brand": "Toyota",
+    "model": "Corolla",
+    "year": 2020
+}
+
+# Add new key
+car["color"] = "Black"
+
+# Update existing key
+car["year"] = 2022
+
+print("Updated dictionary:", car)
+
+#output
+#Updated dictionary: {'brand': 'Toyota', 'model': 'Corolla', 'year': 2022, 'color': 'Black'}
