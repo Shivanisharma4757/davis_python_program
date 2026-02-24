651b42e8ab1394cd407c02ae7a3ed6ecbbcdcdbf
Shivanisharma4757 <shivani.sharma.mca.2025@miet.ac.in>
2026-02-24 11:03:01 +0530

    Create positive_negative_zero.py

---
+++ 24_Feb_class_lab_work_100question_python/positive_negative_zero.py
@@ -0,0 +1,17 @@
+# take number from user
+num = int(input("Enter a number: "))
+# check if the number is positive, negative or zero
+if num > 0:
+    print("the number is positive")
+elif num < 0:
+    print("the number is negative")
+else:
+    print("the number is zero")
+
+#output
+#Enter a number: 5
+#the number is positive
+#Enter a number: -8
+#the number is negative
+#Enter a number: 0
+#the number is zero
