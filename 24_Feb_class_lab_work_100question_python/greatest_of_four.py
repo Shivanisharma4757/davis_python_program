1de30510fad137dfbe3037208712eceffe3776c2
Shivanisharma4757 <shivani.sharma.mca.2025@miet.ac.in>
2026-02-24 14:01:53 +0530

    Create greatest_of_four.py

---
+++ 24_Feb_class_lab_work_100question_python/greatest_of_four.py
@@ -0,0 +1,18 @@
+# Find greatest of four numbers
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+d = int(input("Enter fourth number: "))
+
+# Finding largest
+largest = max(a, b, c, d)
+
+print("Greatest number is:", largest)
+
+#output
+#Enter first number: 5
+#Enter second number: 10
+#Enter third number: 7
+#Enter fourth number: 3
+#Greatest number is: 10
