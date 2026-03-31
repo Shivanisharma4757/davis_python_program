fc0dd09123c4e320d749410f9297ca1b5059f3d9
Shivanisharma4757 <shivani.sharma.mca.2025@miet.ac.in>
2026-03-31 11:03:00 +0530

    Create Find mean, median, and standard deviation of an array.py

---
+++ Python Lab + Assessment.31.march/Find mean, median, and standard deviation of an array.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+# Input: NumPy array
+arr = np.array([10, 20, 30, 40, 50])
+
+# Calculations
+mean = np.mean(arr)
+median = np.median(arr)
+std_dev = np.std(arr)
+
+# Output
+print("Mean:", mean)
+print("Median:", median)
+print("Standard Deviation:", std_dev)
+
+#output
+#Mean: 30.0
+#Median: 30.0
+#Standard Deviation: 14.142135623730951
